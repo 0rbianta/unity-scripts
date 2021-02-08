e2a22aac7a4bce54e6a48ab344b028db880f984a
0rbianta <63746411+0rbianta@users.noreply.github.com>
2021-02-08 09:12:04 +0300

    Add files via upload

---
+++ focus_distance_auto.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class focus_distance_auto : MonoBehaviour
+{
+
+
+    PostProcessVolume post_v;
+    DepthOfField dof;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        post_v = GetComponent<PostProcessVolume>();
+        post_v.profile.TryGetSettings(out dof);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit intersection;
+
+
+        if(Physics.Raycast(/*origin*/transform.position,/*distance*/new Vector3(0, 0, 1) /*<<-- same with Vector3.forward*/, out intersection)){
+            //print(intersection.distance);
+            dof.focusDistance.value = intersection.distance;
+
+        }
+    }
+
+
+
+}
