b5c00f1d7a8dc7f623cce820f5741b66a508cfe7
0rbianta <63746411+0rbianta@users.noreply.github.com>
2021-02-08 12:11:58 +0300

    Add files via upload

---
+++ env_mgr.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class env_mgr : MonoBehaviour
+{
+
+    public bool item1_picked = false;
+    
+    public GameObject player, item1_env_view, item2_env_view, item1_onplayer, item2_onplayer,item1_nottaken,item2_nottaken;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKey("1"))
+        {
+            set_player_item(1);
+        }
+        if(Input.GetKey("2"))
+        {
+            set_player_item(2);
+        }
+
+        listen_item_pick_key_and_set();
+
+    }
+
+    void listen_item_pick_key_and_set(){
+
+        if((item1_nottaken.transform.position - player.transform.position).magnitude < 2f)
+        {
+            //if player near to item1 more than 4f
+
+            if(!check_player_have_item(1))
+            {
+                if(Input.GetKey("k")) //get item
+                {
+                    item1_nottaken.SetActive(false);
+                    set_player_item(1);
+                }
+
+            }else{
+                if(Input.GetKey("q")) //get item
+                {
+                    item1_nottaken.SetActive(true);
+                    set_player_item(0);
+                }
+            }
+
+        }
+        if((item2_nottaken.transform.position - player.transform.position).magnitude < 2f)
+        {
+            //if player near to item1 more than 4f
+
+            if(!check_player_have_item(2))
+            {
+                if(Input.GetKey("k")) //get item
+                {
+                    item2_nottaken.SetActive(false);
+                    set_player_item(2);
+                }
+
+            }else{
+                if(Input.GetKey("q")) //get item
+                {
+                    item2_nottaken.SetActive(true);
+                    set_player_item(0);
+                }
+            }
+
+        }
+
+/*
+        Debug.Log("run");
+
+        if(!check_player_have_item(itemid))
+        {
+            //player have item
+            if(Input.GetKey("k")) //get item
+            {
+                item1_nottaken.SetActive(false);
+                set_player_item(itemid);
+
+            }
+        }else{
+            //player have item
+            if(Input.GetKey("q")) //drop item
+            {
+                item1_nottaken.SetActive(true);
+                set_player_item(0); //reset player item
+
+            }
+        }
+*/
+    }
+
+    void set_player_item(int itemid){
+
+        switch (itemid)
+        {
+            case 0:
+            {
+                item1_env_view.SetActive(false);
+                item1_onplayer.SetActive(false);
+                item2_env_view.SetActive(false);
+                item2_onplayer.SetActive(false);
+                break;
+            }
+            case 1:
+            {
+                item1_env_view.SetActive(true);
+                item1_onplayer.SetActive(true);
+                item2_env_view.SetActive(false);
+                item2_onplayer.SetActive(false);
+                break;
+            }
+            case 2:
+            {
+                item1_env_view.SetActive(false);
+                item1_onplayer.SetActive(false);
+                item2_env_view.SetActive(true);
+                item2_onplayer.SetActive(true);
+                break;
+            }
+
+
+            default:
+            return;
+        }
+
+    }
+
+    bool check_player_have_item(int itemid){
+
+        switch (itemid)
+        {
+            case 0:
+            {
+                if(!item1_env_view.active && !item2_env_view.active)
+                {
+                    return false;
+                }
+                return true;
+            }
+
+            case 1:
+            {
+                if(!item1_env_view.active)
+                {
+                    return false;
+                }
+                return true;
+            }
+            case 2:
+            {
+                if(!item2_env_view.active)
+                {
+                    return false;
+                }
+                return true;
+            }
+
+
+            default:
+            return false;
+        }
+
+    }
+
+}
---
+++ light_mgr.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class light_mgr : MonoBehaviour
+{
+
+    public int sleep_seconds = 1;
+    private bool is_light1_controllable = true;
+    public GameObject player, light1, ligth1_on, ligth1_off;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(is_light1_controllable)
+        {
+            if((ligth1_on.transform.position - player.transform.position).magnitude < 2f)
+            {
+                //player near to light more than 2f
+                if(Input.GetKey("k")) //Open/close light
+                {
+                    light_activity(light1, ligth1_on, ligth1_off);
+                    is_light1_controllable=false;
+                    StartCoroutine(light_controllable_changer());
+                }
+            }
+        }
+        
+    }
+
+    void light_activity(GameObject light, GameObject light_on, GameObject light_off){
+
+        if(!light.active)
+        {
+            light_on.SetActive(true);
+            light_off.SetActive(false);
+            light.SetActive(true);
+        }else{
+            light_on.SetActive(false);
+            light_off.SetActive(true);
+            light.SetActive(false);
+        }
+
+    }
+
+    IEnumerator light_controllable_changer(){
+        yield return new WaitForSeconds(sleep_seconds);
+        is_light1_controllable=true;
+    }
+
+
+
+}
