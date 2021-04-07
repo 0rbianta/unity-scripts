0e215f9f90fb9cb58e5b0141b1038644b3478879
0rbianta <63746411+0rbianta@users.noreply.github.com>
2021-04-07 09:48:50 +0300

    Update focus_distance_auto.cs

--- focus_distance_auto.cs
+++ focus_distance_auto.cs
@@ -6,10 +6,11 @@ using UnityEngine.Rendering.PostProcessing;
 public class focus_distance_auto : MonoBehaviour
 {
 
-
+    GameObject[] invisible_objs;
     PostProcessVolume post_v;
     DepthOfField dof;
     public GameObject cam;
+    string inv_tag_name = "invisible";
 
 
     // Start is called before the first frame update
@@ -17,6 +18,8 @@ public class focus_distance_auto : MonoBehaviour
     {   
         post_v = cam.GetComponent<PostProcessVolume>();
         post_v.profile.TryGetSettings(out dof);
+
+        invisible_objs = GameObject.FindGameObjectsWithTag(inv_tag_name);
     }
 
     // Update is called once per frame
@@ -25,9 +28,17 @@ public class focus_distance_auto : MonoBehaviour
         RaycastHit intersection;
         
 
-        if(Physics.Raycast(/*origin*/transform.position,/*distance*/transform.TransformDirection(Vector3.forward), out intersection)){
+        if(Physics.Raycast(/*origin*/transform.position,/*distance*/transform.TransformDirection(Vector3.forward) /*<<-- same with Vector3.forward*/, out intersection)){
             //print(intersection.distance);
-            dof.focusDistance.value = intersection.distance;
+
+            for(int i=0;i<invisible_objs.Length;i++){
+                if (!((transform.TransformDirection(Vector3.forward).z - invisible_objs[i].transform.TransformDirection(Vector3.forward).z) < 4f)){
+
+                
+                    dof.focusDistance.value = intersection.distance;
+                }
+            }
+
 
         }
     }
